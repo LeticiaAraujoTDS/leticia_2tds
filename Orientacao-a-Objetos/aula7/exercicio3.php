f87ab8d4d01ab1d96ce2d43df7cac8a043738c30
Leticia Correa <leticia.araujo.tds2023@gmail.com>
2024-07-04 15:34:11 -0300

    .

---
+++ Orientacao-a-Objetos/aula7/exercicio3.php
@@ -0,0 +1,17 @@
+<?php
+
+$turmas = array();
+
+//Ler as turmas
+for ($i=0; $i < 4; $i++) { 
+    $turma = array();
+    $turma["curso"] = readline("Informe o curso da turma " . $i . ": ");
+    $turma["ano"] = readline("Informe o ano da turma " . $i . ": ");
+    $turma["qtdAlunos"] = readline("Informe a quantidade de alunos da turma " . $i . ": ");
+    array_push($turmas, $turma);
+}
+$a = 0;
+for ($i=0; $i < 4; $i++) { 
+    $a = $a + $turmas[$i]["qtdAlunos"];
+}
+print ("\nA quantidade de alunos é " . $a . ".\n");
